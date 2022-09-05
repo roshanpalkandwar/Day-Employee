c02da0a9b84d4b0630f9b4abdce8b420a9b0c722
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-05 23:37:35 +0530

    [roshan]EMP present or absent

---
+++ Day8EmployeeRoll/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day8EmployeeRoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welecome Employee Wages");
+            Console.WriteLine("Choose Option\n 1. Check absent present");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    EmpPersentAbsent.EmployeeAttendance();
+                    break;
+            }
+        }
+    }
+}
