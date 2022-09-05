5038c929ace1dfff2710bb45d94899f0f71c48a2
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-05 23:43:55 +0530

    [roshan]employee daily wages

--- Day8EmployeeRoll/Program.cs
+++ Day8EmployeeRoll/Program.cs
@@ -5,12 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome Employee Wages");
-            Console.WriteLine("Choose Option\n 1. Check absent present");
+            Console.WriteLine("Choose Option\n 1. Check absent present\n2.daily wages");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     EmpPersentAbsent.EmployeeAttendance();
+                    break;
+                    case 2:
+                    dailyWages.CalculateDailyWage();
                     break;
             }
         }
