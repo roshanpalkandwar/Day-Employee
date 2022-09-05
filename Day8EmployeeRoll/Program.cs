16d99483347ecb91652eb98aa5499549fbe33c04
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-06 00:26:22 +0530

    [roshan]emp wages

--- Day8EmployeeRoll/Program.cs
+++ Day8EmployeeRoll/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("welecome Employee Wages");
-            Console.WriteLine("Choose Option\n 1. Check absent present\n2.daily wages\n3.part time wages\n4.switch loop\n5. monthly wages");
+            Console.WriteLine("Choose Option\n 1. Check absent present\n2.daily wages\n3.part time wages\n4.switch loop\n5. monthly wages\n6.Monthly Wages using while loop\n7.emp wages");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +24,13 @@
                 case 5:
                     WagesMonthly.CalculateWageForMonth();
                     break;
+                case 6:
+                    WagesWithWhileLop.CalculateWage();
+                    break;
+                case 7:
+                    ComputeEmpWages.ComputeEmpWage();
+                    break ;
+                   
             }
         }
     }
